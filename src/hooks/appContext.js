472e863cee924d05d1c4d0c11c05f574964f77f2
JoLeaper <joleap123@gmail.com>
2020-08-04 14:51:11 -0700

    hooks written for appContext

--- src/hooks/appContext.js
+++ src/hooks/appContext.js
@@ -1,3 +1,28 @@
 import React, { useContext } from 'react';
 
 export const AppContext = React.createContext();
+
+export const useState = () => {
+  const { state } = useContext(AppContext);
+  return state;
+};
+
+export const useDispatch = () => {
+  const dispatch = useContext(AppContext);
+  return dispatch;
+};
+
+export const userUserName = () => {
+  const { userName } = useContext(AppContext);
+  return userName;
+};
+
+export const useUserInfo = () => {
+  const { userInfo } = useContext(AppContext);
+  return userInfo;
+};
+
+export const useRepos = () => {
+  const { userRepos } = useContext(AppContext);
+  return userRepos;
+};
