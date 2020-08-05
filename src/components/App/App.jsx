4730cabdfd3e219d084693834ed58e471ce04ee1
JoLeaper <joleap123@gmail.com>
2020-08-04 17:04:17 -0700

    userInfo function rendering with data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import SearchBar  from '../SearchBar/SearchBar'; 
+import SearchBar  from '../SearchBar/SearchBar';
+import UserInfo from '../UserInfo/UserInfo';
 
 export default function App() {
   return (
-    <SearchBar />
+    <>
+      <SearchBar />
+      <UserInfo />
+    </>
   );
 }
   
