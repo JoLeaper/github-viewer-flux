195c82ad564536e03db4e63ebca760cfd2841648
JoLeaper <joleap123@gmail.com>
2020-08-04 17:08:34 -0700

    RepoList displaying list of repos

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SearchBar  from '../SearchBar/SearchBar';
 import UserInfo from '../UserInfo/UserInfo';
+import RepoList from '../RepoList/RepoList';
 
 export default function App() {
   return (
     <>
       <SearchBar />
       <UserInfo />
+      <RepoList />
     </>
   );
 }
